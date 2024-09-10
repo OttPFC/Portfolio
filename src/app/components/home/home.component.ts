85a7f0994dda9d8782b6baf03d4c2dbc4b2aab06
OttPFC <ale.maz097@gmail.com>
2024-09-10 16:33:19 +0200

    animation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  adjectives = [
+    { name: 'JavaScript', logo: '/assets/img/js2_logo.png' },
+    { name: 'Angular', logo: '/assets/img/angular_logo.png' },
+    { name: 'Java', logo: '/assets/img/java_image.png' },
+    { name: 'Spring Boot', logo: '/assets/img/spring2_logo.png' },
+    { name: 'PostgreSQL', logo: '/assets/img/postgres_logo2.png' }
+  ];
 
+  currentIndex = 0;
+  currentAdjective = this.adjectives[this.currentIndex];
+
+  ngOnInit(): void {
+    this.startTextRotation();
+  }
+
+  startTextRotation() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.adjectives.length;
+      this.currentAdjective = this.adjectives[this.currentIndex];
+    }, 2000); // Cambia ogni 2 secondi
+  }
 }
--- src/app/main-components/header/header.component.ts
+++ src/app/main-components/header/header.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
 
   isCollapsed = false;
+  adjectives = ['Full Stack Developer', 'BackEnd Developer','FrontEnd Developer', 'Java Developer'];
+  currentIndex = 0;
+  currentAdjective = this.adjectives[this.currentIndex];
+
+  ngOnInit(): void {
+    this.startTextRotation();
+  }
+
+  startTextRotation() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.adjectives.length;
+      this.currentAdjective = this.adjectives[this.currentIndex];
+    }, 3000);
+  }
 }
+
