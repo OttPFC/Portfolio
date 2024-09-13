8e6da66c47684ecab6f607a2edd4081412a2fdb5
OttPFC <ale.maz097@gmail.com>
2024-09-13 18:25:07 +0200

    about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: 'about', component: AboutComponent,
+  },
+  {
+    path: 'projects', component: ProjectsComponent,
   }
 ];
 
