a9685200edcbed516d19fd3e3f37def0fa7d2ec4
OttPFC <ale.maz097@gmail.com>
2024-10-01 12:14:51 +0200

    project layout fixed

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -13,15 +13,28 @@ export class ProjectsComponent {
     number3: 0,
     number4: 0,
     number5: 0,
-    number6: 0
+    number6: 0,
+    number7: 0,
+    number8: 0,
+    number9: 0,
+    number10: 0,
+    number11: 0,
+    number12: 0
   };
 
   ngOnInit() {
     this.animateCounter('number1', 6, 3000);
-    this.animateCounter('number2', 80, 1700);
-    this.animateCounter('number3', 81, 3100);
-    this.animateCounter('number4', 16, 3450);
+    this.animateCounter('number2', 10, 1700);
+    this.animateCounter('number3', 80, 3100);
+    this.animateCounter('number4', 10, 3450);
     this.animateCounter('number5', 47, 3300);
+    this.animateCounter('number6', 5, 3000);
+    this.animateCounter('number7', 10, 1700);
+    this.animateCounter('number8', 80, 3100);
+    this.animateCounter('number9', 10, 3450);
+    this.animateCounter('number10', 32, 3300);
+    this.animateCounter('number11', 47, 3300);
+    this.animateCounter('number12', 47, 3300);
   }
 
 
